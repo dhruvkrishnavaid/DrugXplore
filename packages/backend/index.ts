cd33e01cdd70bf5c09e5f42bf72802a4cb189856
Dhruv Krishna Vaid <dhruvkrishnavaid@gmail.com>
2025-11-29 11:33:38 +0530

    Add hpp protection

--- packages/backend/index.ts
+++ packages/backend/index.ts
@@ -1,5 +1,6 @@
 import bodyParser from "body-parser";
 import cors from "cors";
+import hpp from "hpp";
 import "dotenv/config";
 import express from "express";
 import ayurvedaRouter from "./src/routes/ayurveda";
@@ -11,6 +12,7 @@ import greetRouter from "./src/routes/greet";
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(hpp());
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
