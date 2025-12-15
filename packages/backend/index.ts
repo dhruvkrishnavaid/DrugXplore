ed4c4ca21451b0d1d7398e3a5d4ca51f0d9cd4e3
Dhruv Krishna Vaid <dhruvkrishnavaid@gmail.com>
2025-12-15 20:23:52 +0530

    Remove credentials support in cors

--- packages/backend/index.ts
+++ packages/backend/index.ts
@@ -17,7 +17,6 @@ app.use(
   cors({
     origin: ["http://localhost:5173", "https://drugxplore.web.app"],
     optionsSuccessStatus: 200,
-    credentials: true,
   }),
 );
 app.use(bodyParser.json());
