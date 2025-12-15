1c1caaa4c6cc094c2739616f6cc9028aab3bd5e2
Dhruv Krishna Vaid <dhruvkrishnavaid@gmail.com>
2025-12-15 21:01:30 +0530

    Enable cors for preflight

--- packages/backend/index.ts
+++ packages/backend/index.ts
@@ -12,13 +12,14 @@ import greetRouter from "./src/routes/greet";
 const app = express();
 const port = process.env.PORT || 3000;
 
+const corsOptions = {
+  origin: ["http://localhost:5173", "https://drugxplore.web.app"],
+  optionsSuccessStatus: 200,
+};
+
 app.use(hpp());
-app.use(
-  cors({
-    origin: ["http://localhost:5173", "https://drugxplore.web.app"],
-    optionsSuccessStatus: 200,
-  }),
-);
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
